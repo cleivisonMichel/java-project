7d11d3b2812d16d290ccab342d838ff110db4671
MichelDev <cleivisonmicheldev@gmail.com>
2022-11-24 21:15:41 -0300

    Mentoria e Curso

---
+++ Dio-Java-Poo/src/br/com/dio/dominio/Main.java
@@ -0,0 +1,27 @@
+package br.com.dio.dominio;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descriçãp curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Descriçãp curso Pyton");
+        curso2.setCargaHoraria(40);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
